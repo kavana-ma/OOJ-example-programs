9a793f5509577f71acb1ca6a60575ecc887af209
Kavana M A <kavanama.cs23@bmsce.ac.in>
2024-09-30 22:55:18 +0530

    Create twodimarray.java

---
+++ twodimarray.java
@@ -0,0 +1,19 @@
+class twodimarray{
+public static void main(String args[]){
+int twod[][]=new int[4][5];
+int i,j,k=0;
+for(i=0;i<4;i++){
+   for(j=0;j<5;j++){
+      twod[i][j]=k;
+      k++;
+   }
+}
+for(i=0;i<4;i++){
+   for(j=0;j<5;j++){
+      System.out.println(twod[i][j]+"");
+   }
+   System.out.println();
+}
+
+}
+}
