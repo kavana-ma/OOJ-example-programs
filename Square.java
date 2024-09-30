c435267b35d8548a6f952ae43b258ad56470e509
Kavana M A <kavanama.cs23@bmsce.ac.in>
2024-10-01 00:25:38 +0530

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,22 @@
+public class Square{
+int side;
+
+Square(){
+side=45;
+}
+
+void calArea(){
+int area=side*side;
+System.out.println("Area:"+area);
+}
+void calPerimeter(){
+int perimeter=4*side;
+System.out.println("Perimeter:"+perimeter);
+}
+
+public static void main(String args[]){
+Square sq=new Square();
+sq.calArea();
+sq.calPerimeter();
+}
+}
